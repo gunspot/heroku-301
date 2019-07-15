6bbbcc3fba3e6a39053b7eb9836f46afb784f6e5
Ben <ben@fbstudios.com>
2019-07-15 13:40:46 -0500

    Redirect

--- redirect/urls.py
+++ redirect/urls.py
@@ -1,22 +1,18 @@
-"""redirect URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.shortcuts import redirect
+
+# Get domain
+from .domain import redirect_domain
+
+
+
+def redirect_view(request):
+    response = redirect('https://'+redirect_domain)
+    response.status_code = 301
+    return response
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path('.*', redirect_view)
 ]
